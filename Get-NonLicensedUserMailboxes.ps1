43a8e750ca77b7c0c2446edb139ab8d73bbe4cb7
Sam Murcio <sam.murcio@gmail.com>
2016-10-12 10:55:45 -0500

    Post script optimization

--- Get-NonLicensedUserMailboxes.ps1
+++ Get-NonLicensedUserMailboxes.ps1
@@ -1,16 +1,70 @@
-﻿
-$i = 0
-$out = @()
-$csv = $env:USERPROFILE + "\Desktop\NonLicensed_MailboxUser_Report.csv"
+﻿param( 	
+  [switch]$EmailResults,
+  [ValidateRange(0,10)]
+  [int32]$CreatedDaysBack
+)
+
+$user = "admin-smurcio@Valeant.onmicrosoft.com"
+$keyFile = "C:\O365 License Script\key.txt"
+$pwdFile = "C:\O365 License Script\cred.txt"
+$url="https://outlook.office365.com/powershell-liveid/"
+$pwd = Get-Content $pwdFile | ConvertTo-SecureString -Key (Get-Content $keyFile)
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $pwd
+$date = Get-Date -Format MMddyyyy
+$csv = "C:\O365 License Script\Reports\" + $date + "_NonLicensed_MailboxUser_Report.csv"
+
+
+function sendReport {
+  $subject = "Non-Licensed User Mailbox Report $date"
+  #$recipient = "o365report@valeant.com","nsylvester@westmonroepartners.com","ksullivan@westmonroepartners.com","jvancleve@westmonroepartners.com"
+  $recipient = "smurcio@westmonroepartners.com", "jvancleve@westmonroepartners.com"
+  $sender = "Exchange@valeant.com"
+  $server = "SMTPRELAY.valeant.com"
+  Send-MailMessage -SMTPServer $server -To $recipient -From $sender -Subject $subject `
+  -Body "Please see the attached results file." -Attachments $csv
+}
+
+function sendError {
+  $subject = "ERROR: Non-Licensed User Mailbox Report $date"
+  #$recipient = "o365report@valeant.com","nsylvester@westmonroepartners.com","ksullivan@westmonroepartners.com","jvancleve@westmonroepartners.com"
+  $recipient = "smurcio@westmonroepartners.com"
+  $sender = "Exchange@valeant.com"
+  $server = "SMTPRELAY.valeant.com"
+  Send-MailMessage -SMTPServer $server -To $recipient -From $sender -Subject $subject `
+  -Body "Script is unable to run due to following error, please investigate: `n $error"
+}
+
+
+Get-PSSession | Remove-PSSession
+Import-Module MSOnline
+try { Connect-MSOLService -Credential $creds -ErrorAction Stop } Catch {$error = $_; sendError; Exit }
+$exoSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $url -Credential $creds `
+-Authentication Basic -AllowRedirection -Name "EXO"
+try { Import-PSSession $exoSession -AllowClobber -ErrorAction Stop } Catch { $error = $_; sendError; Exit }
+
+$test = Get-MsolCompanyInformation -ErrorAction SilentlyContinue
+$exoSession = Get-PSSession -Name "EXO"
+
+if ( $test -eq $null -and $exoSession.State -ne "Opened" -and $exoSession.Availability -ne "Available" ) {
+  $error = "Unable to connect to one or more of the following services:`n  -MSOL`n  -ExchangeOnline"
+  sendError 
+}
 
 $activity = "Processing Request..."
 $status = "Getting All Mailboxes" 
-
 Write-Progress -Activity $activity -Status $status -ID 1
-$mbxs = Get-mailbox
+$mbxs = Get-Mailbox #-ResultSize Unlimited
+
+if ( $createddaysback ) {
+  $checkDate = (Get-Date).AddDays(-$createddaysback)
+  $mbxs = $mbxs | ? { $_.WhenMailboxCreated -ge $checkDate }
+}
 $userMbxs = $mbxs | ? {$_.RecipientTypeDetails -eq "UserMailbox"}
+Clear-Variable mbxs
 Write-Progress -Activity $activity -Status $status -ID 1 -Completed
 
+$i = 0
+$out = @()
 $userCount = $userMbxs.Count
 foreach ( $a in $userMbxs ) {
   $i++
@@ -33,10 +87,16 @@ foreach ( $a in $userMbxs ) {
       Username = $a.UserPrincipalName
       DisplayName = $a.DisplayName
       Result = $result
+      Mailbox_Creation = $a.WhenMailboxCreated
     }
     $out += $obj
   }
 
 }
 
+Get-PSSession | Remove-PSSession
 $out | Export-Csv -NoTypeInformation $csv
+
+if ( $emailresults ) {
+  sendReport
+}
