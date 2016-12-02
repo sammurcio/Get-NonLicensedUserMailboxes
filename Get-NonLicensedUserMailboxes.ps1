7fb67c927a05c4ce4a1626713adbe513256f4ee7
Samuel Murcio <sam.murcio@gmail.com>
2016-12-02 11:06:09 -0600

    V1.1

--- Get-NonLicensedUserMailboxes.ps1
+++ Get-NonLicensedUserMailboxes.ps1
@@ -1,35 +1,35 @@
-﻿param( 	
+param( 	
   [switch]$EmailResults,
   [ValidateRange(0,10)]
   [int32]$CreatedDaysBack
 )
 
-$user = "admin-smurcio@Valeant.onmicrosoft.com"
+# Variables that can be modified:
+#=================================================================================
+$csv = "C:\O365\Reports\" + $date + "_NonLicensed_MailboxUser_Report.csv"
+$checkLic = "WESTMONROEPARTNERS1:ENTERPRISEPREMIUM_NOPSTNCONF"
+#Variables if email results is enabled
+$recipient = "smurcio@domain.com", "user@domain.com"
+$sender = "Exchange@domain.com"
+$server = "SMTPRELAY.domain.com"
+#=================================================================================
+
+$user = "asfa@domain.onmicrosoft.com"
 $keyFile = "C:\O365 License Script\key.txt"
 $pwdFile = "C:\O365 License Script\cred.txt"
 $url="https://outlook.office365.com/powershell-liveid/"
 $pwd = Get-Content $pwdFile | ConvertTo-SecureString -Key (Get-Content $keyFile)
 $creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $pwd
 $date = Get-Date -Format MMddyyyy
-$csv = "C:\O365 License Script\Reports\" + $date + "_NonLicensed_MailboxUser_Report.csv"
-
 
 function sendReport {
   $subject = "Non-Licensed User Mailbox Report $date"
-  #$recipient = "o365report@valeant.com","nsylvester@westmonroepartners.com","ksullivan@westmonroepartners.com","jvancleve@westmonroepartners.com"
-  $recipient = "smurcio@westmonroepartners.com", "jvancleve@westmonroepartners.com"
-  $sender = "Exchange@valeant.com"
-  $server = "SMTPRELAY.valeant.com"
   Send-MailMessage -SMTPServer $server -To $recipient -From $sender -Subject $subject `
   -Body "Please see the attached results file." -Attachments $csv
 }
 
 function sendError {
   $subject = "ERROR: Non-Licensed User Mailbox Report $date"
-  #$recipient = "o365report@valeant.com","nsylvester@westmonroepartners.com","ksullivan@westmonroepartners.com","jvancleve@westmonroepartners.com"
-  $recipient = "smurcio@westmonroepartners.com"
-  $sender = "Exchange@valeant.com"
-  $server = "SMTPRELAY.valeant.com"
   Send-MailMessage -SMTPServer $server -To $recipient -From $sender -Subject $subject `
   -Body "Script is unable to run due to following error, please investigate: `n $error"
 }
@@ -51,51 +51,61 @@ if ( $test -eq $null -and $exoSession.State -ne "Opened" -and $exoSession.Availa
 }
 
 $activity = "Processing Request..."
-$status = "Getting All Mailboxes" 
+$status = "Getting All User Mailboxes" 
 Write-Progress -Activity $activity -Status $status -ID 1
-$mbxs = Get-Mailbox #-ResultSize Unlimited
-
+$mbxs = Get-Mailbox -Filter "RecipientTypeDetails -eq 'UserMailbox'" -ResultSize Unlimited
 if ( $createddaysback ) {
   $checkDate = (Get-Date).AddDays(-$createddaysback)
   $mbxs = $mbxs | ? { $_.WhenMailboxCreated -ge $checkDate }
 }
-$userMbxs = $mbxs | ? {$_.RecipientTypeDetails -eq "UserMailbox"}
-Clear-Variable mbxs
 Write-Progress -Activity $activity -Status $status -ID 1 -Completed
 
 $i = 0
-$out = @()
-$userCount = $userMbxs.Count
-foreach ( $a in $userMbxs ) {
+$mbxCount = $mbxs.Count
+foreach ( $a in $mbxs ) {
   $i++
-  Write-Progress -Activity "Processing mailbox list" -Status "Currently on mailbox $($a.DisplayName)" -PercentComplete ($i / $userCount * 100)
-  $msolUser = Get-MsolUser -UserPrincipalName $a.UserPrincipalName
-  if ( $msolUser.IsLicensed -eq $false) {
+  if ( $msolUser ) { Clear-Variable msolUser }
+  $upn = $a.UserPrincipalName
+  $displayName = $a. displayName
+  $created = $a.WhenMailboxCreated
+  Write-Progress -Activity "Processing mailbox list" -Status "Currently on mailbox $displayName" -PercentComplete ($i / $mbxCount * 100)
+  $msolUser = Get-MsolUser -UserPrincipalName $upn -ErrorAction SilentlyContinue
+
+  if ( !$msolUser ) {
+    $result = "MSOL User Not Found"
+  } elseif ( $msolUser.IsLicensed -eq $false) {
     $result = "User is not licensed"
-  } else {
-    
-    if ( $msolUser.Licenses.ServiceStatus.ServicePlan.ServiceName -like "Exchange*" ) {
-      $result = "User is licensed for Exchange Online"
+  } elseif ( $msolUser.IsLicensed -eq $true ) {
+
+    if ( $msolUser.Licenses.AccountSkuId -contains $checkLic ) {
+      $serviceStatus = $msolUser.Licenses |`
+      ? { $_.AccountSkuid -eq $checkLic } | Select -ExpandProperty ServiceStatus |`
+      ? {$_.serviceplan.servicename -like "EXCHANGE*" -and $_.serviceplan.servicename -notlike "EXCHANGE_ANALYTICS"}
+      $isDisabled = $serviceStatus.ProvisioningStatus -contains "Disabled"
+      
+      if ( $isDisabled -eq $true ) {
+        $result = "User is licensed with SKU $($checkLic) but service is not enabled"
+      } else {
+         $result = "User is licensed with SKU $($checkLic) and Exchange Online service is enabled"
+      }
+
     } else {
-      $result = "User is not licensed for Exchange Online"
+      $result = "User is licensed but not with SKU $($checkLic)"
     }
 
   }
 
-  if ( $result -match "User is not licensed" ) {
-    $obj = [pscustomobject]@{
-      Username = $a.UserPrincipalName
-      DisplayName = $a.DisplayName
-      Result = $result
-      Mailbox_Creation = $a.WhenMailboxCreated
+  $obj = [pscustomobject]@{
+    Username = $upn
+    DisplayName = $displayName
+    Result = $result
+    Mailbox_Created = $created
     }
-    $out += $obj
-  }
+    $obj | Export-Csv -Path $csv -NoTypeInformation -Append
 
 }
 
 Get-PSSession | Remove-PSSession
-$out | Export-Csv -NoTypeInformation $csv
 
 if ( $emailresults ) {
   sendReport
